c22481c909aee12496f207471013e952e791ea8f
porcodioft <kingfireketer@gmail.com>
2025-02-21 12:42:28 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -125,8 +125,8 @@ global.tiktok = 'https://www.instagram.com/filippotozzii'
 global.fb = 'https://www.facebook.com/elrebelde21'
 global.faceb = 'https://facebook.com/groups/872989990425789/'
 
-global.n2 = "https://whatsapp.com/channel/0029VaWABAMG8l5K8K9PAB" //canal cortaba 
-global.nna = 'https://whatsapp.com/channel/0029Va4QjH7DeON0ePwzjS1A' //Update 
+global.n2 = "https://whatsapp.com/channel/0029Vb0j8QX84OmFZZsJKW15" //canal cortaba 
+global.nna = 'https://whatsapp.com/channel/0029Vb0j8QX84OmFZZsJKW15' //Update 
 global.nn = 'https://chat.whatsapp.com/HOcczbax7HR1TgRcLfeBRE' //LoliBot
 global.nn2 = 'https://chat.whatsapp.com/HOcczbax7HR1TgRcLfeBRE' //Loli & Nova
 global.nn3 = 'https://chat.whatsapp.com/HOcczbax7HR1TgRcLfeBRE' //Grupo de Colaboracion
